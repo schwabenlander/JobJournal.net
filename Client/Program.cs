6bda5cc9d688c0ade578e58e8116c9d5b4a1f342
Sean Schwabenlander <sean@schwabenlander.com>
2021-11-21 17:10:42 -0600

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using JobJournal.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("JobJournal.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("JobJournal.ServerAPI"));
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://jobjournal.onmicrosoft.com/99d05c09-48d8-4448-a6bb-10e076dd6b00/API.Access");
+    options.ProviderOptions.LoginMode = "redirect";
+});
+
+await builder.Build().RunAsync();
