adb1170c3c9d658bf48b5fb408ec635fac113c02
Sean Schwabenlander <sean@schwabenlander.com>
2021-11-22 15:37:05 -0600

    Applied new layout

---
+++ Client/wwwroot/js/offcanvas.js
@@ -0,0 +1,7 @@
+function bindOffcanvasToggle() {
+  'use strict'
+
+  document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  })
+};
